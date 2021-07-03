b2eae1c41590938217e9209e1b352e350e23b6ad
Yana <yana.harutyunyan.s@tumo.org>
2021-07-03 16:50:42 +0400

    nor file

---
+++ script.js
@@ -0,0 +1,117 @@
+function generator(matLen, gr, grEata, pr, cb) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEata; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }
+    for (let i = 0; i < pr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+
+        for (let i = 0; i < cb; i++) {
+            let x = Math.floor(Math.random() * matLen);
+            let y = Math.floor(Math.random() * matLen);
+            if (matrix[x][y] == 0) {
+                matrix[x][y] = 4;
+            }
+        }
+    }
+    return matrix;
+}
+
+let side = 20;
+
+let matrix = generator(50, 120, 50, 95, 1);
+
+var grassArr = []
+var grassEaterArr = []
+var predatorArr = []
+
+function setup() {
+    frameRate(24);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+
+
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y);
+                grassArr.push(gr)
+            } else if (matrix[y][x] == 2) {
+                var grEat = new GrassEater(x, y)
+                grassEaterArr.push(grEat)
+            }
+            else if (matrix[y][x] == 3) {
+                var predat = new Predator(x, y)
+                predatorArr.push(predat)
+            }
+
+
+        }
+    } console.log(grassArr)
+}
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            } else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            } else if (matrix[y][x] == 3) {
+                fill("blue");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 4) {
+                fill("black");
+                rect(x * side, y * side, side, side);
+            }
+
+
+
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].mul();
+
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul();
+        grassEaterArr[i].eat();
+    }
+    for (var i in predatorArr) {
+        predatorArr[i].mul();
+        predatorArr[i].eat();
+    }
+}
+
