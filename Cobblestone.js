f13210fab595d803ddb412b736fe09514d52c71e
Yana <yana.harutyunyan.s@tumo.org>
2021-07-03 18:23:44 +0400

    nvqwertyuio

--- Cobblestone.js
+++ Cobblestone.js
@@ -1,31 +1,8 @@
-class Cobblestone {
-    constructor(x, y, ) {
-        this.x = x;
-        this.y = y;
-        this.multiplay = 0;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-
+class Cobblestone extends livingcreator {
+    constructor(x, y,) {
+        super.constructor()
     }
-    chooseCell(character) {
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+    chooseCell(character) { 
+        super.chooseCell(x,y)
     }
 }
